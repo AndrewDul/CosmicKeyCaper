d681f151ec6b18c07e7b49b96c218daf709f8d9d
AndrewDul <dulandrew.dev@gmail.com>
2025-03-05 23:35:37 +0000

    Initial Structure

--- frontend/js/lvl1.js
+++ frontend/js/lvl1.js
@@ -4,19 +4,19 @@ class Level1 {
 
     // Paralaksa
     this.bgImage = new Image();
-    this.bgImage.src = "assets/images/parallax1.png";
+    this.bgImage.src = "/frontend/assets/images/parallax.png";
 
     // Klucze i monety
     this.keys = [
-      new Item(200, 300, "assets/images/key.png"),
-      new Item(400, 200, "assets/images/key.png"),
-      new Item(600, 250, "assets/images/key.png"),
+      new Item(200, 300, "/frontend/assets/images/key.png"),
+      new Item(400, 200, "/frontend/assets/images/key.png"),
+      new Item(600, 250, "/frontend/assets/images/key.png"),
     ];
 
     this.coins = [
-      new Item(150, 350, "assets/images/coin.png"),
-      new Item(350, 400, "assets/images/coin.png"),
-      new Item(500, 450, "assets/images/coin.png"),
+      new Item(150, 350, "/frontend/assets/images/coin.png"),
+      new Item(350, 400, "/frontend/assets/images/coin.png"),
+      new Item(500, 450, "/frontend/assets/images/coin.png"),
     ];
   }
 
